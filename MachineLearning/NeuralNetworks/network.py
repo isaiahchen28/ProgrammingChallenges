60da8acdb7ab8c344c68e1c7d92f1806d4234457
Isaiah Chen <isaiahchen28@gmail.com>
2019-10-07 13:11:29 -0400

    Made changes to network.py

--- MachineLearning/NeuralNetworks/network.py
+++ MachineLearning/NeuralNetworks/network.py
@@ -1,26 +1,23 @@
 '''
-A module to implement the stochastic gradient descent learning
-algorithm for a feedforward neural network. Gradients are calculated
-using backpropagation.
+A module to implement the stochastic gradient descent learning algorithm for a
+feedforward neural network. Gradients are calculated using backpropagation.
 '''
 import numpy as np
 import random
 
 
 class Network(object):
-
     def __init__(self, sizes):
         '''
-        The list "sizes" contains the number of neurons in the
-        respective layers of the network.  For example, if the list
-        was [2, 3, 1] then it would be a three-layer network, with the
-        first layer containing 2 neurons, the second layer 3 neurons,
-        and the third layer 1 neuron.  The biases and weights for the
-        network are initialized randomly, using a Gaussian
-        distribution with mean 0, and variance 1.  Note that the first
-        layer is assumed to be an input layer, and by convention we
-        won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers.
+        The list "sizes" contains the number of neurons in the respective
+        layers of the network. For example, if the list was [2, 3, 1], then it
+        would be a three-layer network, with the first layer containing 2
+        neurons, the second layer 3 neurons, and the third layer 1 neuron. The
+        biases and weights for the network are initialized randomly, using a
+        Gaussian distribution with mean 0, and variance 1. Note that the first
+        layer is assumed to be an input layer, and by convention we won't set
+        any biases for those neurons, since biases are only ever used in
+        computing the outputs from later layers.
         '''
         self.num_layers = len(sizes)
         self.sizes = sizes
@@ -30,7 +27,7 @@ class Network(object):
 
     def feedforward(self, a):
         '''
-        Return the output of the network if "a" is input.
+        Return the output of the network for an input a.
         '''
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a) + b)
@@ -38,14 +35,13 @@ class Network(object):
 
     def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
         '''
-        Train the neural network using mini-batch stochastic
-        gradient descent.  The "training_data" is a list of tuples
-        "(x, y)" representing the training inputs and the desired
-        outputs.  The other non-optional parameters are
-        self-explanatory.  If "test_data" is provided then the
-        network will be evaluated against the test data after each
-        epoch, and partial progress printed out.  This is useful for
-        tracking progress, but slows things down substantially.
+        Train the neural network using mini-batch stochastic gradient descent.
+        The "training_data" is a list of tuples (x, y) representing the
+        training inputs and the desired outputs. The other non-optional
+        parameters are self-explanatory. If "test_data" is provided then the
+        network will be evaluated against the test data after each epoch, and
+        partial progress printed out. This is useful for tracking progress,
+        but slows things down substantially.
         '''
         training_data = list(training_data)
         n = len(training_data)
@@ -67,10 +63,9 @@ class Network(object):
 
     def update_mini_batch(self, mini_batch, eta):
         '''
-        Update the network's weights and biases by applying
-        gradient descent using backpropagation to a single mini batch.
-        The "mini_batch" is a list of tuples "(x, y)", and "eta"
-        is the learning rate.
+        Update the network's weights and biases by applying gradient descent
+        using backpropagation to a single mini batch. The "mini_batch" is a
+        list of tuples (x, y), and "eta" is the learning rate.
         '''
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
@@ -85,33 +80,32 @@ class Network(object):
 
     def backprop(self, x, y):
         '''
-        Return a tuple "(nabla_b, nabla_w)" representing the
-        gradient for the cost function C_x. "nabla_b" and
-        "nabla_w" are layer-by-layer lists of numpy arrays, similar
-        to "self.biases" and "self.weights".
+        Return a tuple (nabla_b, nabla_w) representing the gradient for the
+        cost function C_x. "nabla_b" and "nabla_w" are layer-by-layer lists of
+        NumPy arrays, similar to "self.biases" and "self.weights".
         '''
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        # feedforward
+
+        # Here, we feed forward
         activation = x
-        activations = [x]  # list to store all the activations, layer by layer
-        zs = []  # list to store all the z vectors, layer by layer
+        # Define a list to store all the activations, layer-by-layer
+        activations = [x]
+        # Define a list to store all the z vectors, layer-by-layer
+        zs = []
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation) + b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        # backward pass
+
+        # Now, we do a backward pass
         delta = self.cost_derivative(activations[-1], y) * \
             sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        # Note that the variable l in the loop below is used a little
-        # differently to the notation in Chapter 2 of the book.  Here,
-        # l = 1 means the last layer of neurons, l = 2 is the
-        # second-last layer, and so on.  It's a renumbering of the
-        # scheme in the book, used here to take advantage of the fact
-        # that Python can use negative indices in lists.
+        # Here, l = 1 is the last layer of neurons, l = 2 is the second-last
+        # layer, and so on.
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
@@ -122,10 +116,10 @@ class Network(object):
 
     def evaluate(self, test_data):
         '''
-        Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation.
+        Return the number of test inputs for which the neural network outputs
+        the correct result. Note that the neural network's output is assumed
+        to be the index of whichever neuron in the final layer has the highest
+        activation.
         '''
         test_results = [(np.argmax(self.feedforward(x)), y)
                         for (x, y) in test_data]
@@ -133,15 +127,17 @@ class Network(object):
 
     def cost_derivative(self, output_activations, y):
         '''
-        Return the vector of partial derivatives partial C_x /
-        partial a for the output activations.
+        Return the vector of partial derivatives partial C_x / partial a for
+        the output activations.
         '''
         return (output_activations - y)
 
 
 def sigmoid(z):
+    ''' Sigmoid function '''
     return 1.0 / (1.0 + np.exp(-z))
 
 
 def sigmoid_prime(z):
+    ''' Derivative of the sigmoid function '''
     return sigmoid(z) * (1 - sigmoid(z))
