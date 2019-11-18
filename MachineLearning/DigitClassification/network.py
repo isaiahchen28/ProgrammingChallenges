e1f3fdc2b63b761593fa471325d1e0e4324a67d2
Isaiah Chen <isaiahchen28@gmail.com>
2019-11-18 13:52:23 -0500

    Add keras modules

--- MachineLearning/DigitClassification/network.py
+++ MachineLearning/DigitClassification/network.py
@@ -4,6 +4,7 @@ feedforward neural network. Gradients are calculated using backpropagation.
 '''
 import numpy as np
 import random
+import mnist_loader
 
 
 class Network(object):
@@ -141,3 +142,14 @@ def sigmoid(z):
 def sigmoid_prime(z):
     ''' Derivative of the sigmoid function '''
     return sigmoid(z) * (1 - sigmoid(z))
+
+
+if __name__ == "__main__":
+    # Load in MNIST data using mnist_loader.py
+    training, validation, test = mnist_loader.load_data_wrapper()
+    training = list(training)
+    # Set up a network with 100 hidden neurons
+    net = Network([784, 100, 10])
+    # Use stochastic gradient descent to learn over 30 epochs
+    # with a mini-batch size of 10 and a learning rate of 3.0
+    net.SGD(training, 30, 10, 3.0, test_data=test)
--- MachineLearning/DigitClassification/test.py
+++ MachineLearning/DigitClassification/test.py
@@ -1,13 +1,23 @@
-# http://neuralnetworksanddeeplearning.com/chap1.html
-import mnist_loader
-import network
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Dropout, Activation
+from keras.utils import np_utils
+
 
 if __name__ == "__main__":
-    # Load in MNIST data using mnist_loader.py
-    training, validation, test = mnist_loader.load_data_wrapper()
-    training = list(training)
-    # Set up a network with 100 hidden neurons
-    net = network.Network([784, 100, 10])
-    # Use stochastic gradient descent to learn over 30 epochs
-    # with a mini-batch size of 10 and a learning rate of 3.0
-    net.SGD(training, 30, 10, 3.0, test_data=test)
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    fig = plt.figure()
+    for i in range(9):
+        plt.subplot(3, 3, i + 1)
+        plt.tight_layout()
+        plt.imshow(X_train[i], cmap='gray', interpolation='none')
+        plt.title("Digit: {}".format(y_train[i]))
+        plt.xticks([])
+        plt.yticks([])
+    fig
